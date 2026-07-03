a86412ed3e7f23913419a5c5bd358034a77f9b97
vahidfarazi <dr.vahidfarazi@gmail.com>
2026-07-03 14:44:33 +0330

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,25 @@
-# بعداً تکمیل می‌شود
+def main_keyboard():
+
+    return {
+        "keyboard":[
+            [{"text":"🔌 نصب انشعاب جدید"}],
+            [{"text":"🔧 اصلاح سرویس"}],
+            [{"text":"⚡ آزمایش و تست کنتور"}],
+            [{"text":"🧾 بررسی صورتحساب برق"}],
+            [{"text":"📖 راهنما"}]
+        ],
+        "resize_keyboard":True
+    }
+
+
+def back_keyboard():
+
+    return {
+        "keyboard":[
+            [
+                {"text":"⬅ بازگشت"},
+                {"text":"🏠 منوی اصلی"}
+            ]
+        ],
+        "resize_keyboard":True
+    }
