a74afec5f5ac75f75116ae07ce1e833402b97ca9
vahidfarazi <dr.vahidfarazi@gmail.com>
2026-07-03 17:03:33 +0330

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,24 @@
+REQUEST_NO = "شماره تقاضا"
+
+MOBILE = "شماره موبایل"
+
+NATIONAL_CODE = "کد ملی"
+
+COMPUTER_CODE = "رمز رایانه"
+
+BILL_ID = "شناسه قبض"
+
+
+IDENTIFIER_NAMES = {
+
+    "REQUEST_NO": REQUEST_NO,
+
+    "MOBILE": MOBILE,
+
+    "NATIONAL_CODE": NATIONAL_CODE,
+
+    "COMPUTER_CODE": COMPUTER_CODE,
+
+    "BILL_ID": BILL_ID
+
+}
