5ba868e904f7567ef841ca9081de9c4d963cc532
vahidfarazi <dr.vahidfarazi@gmail.com>
2026-07-03 21:24:13 +0330

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,1 +1,23 @@
+from datetime import datetime
 
+from core.settings import (
+    WORK_START,
+    WORK_END,
+    WEEKEND,
+)
+
+
+def is_work_time():
+
+    now = datetime.now()
+
+    if now.weekday() in WEEKEND:
+        return False
+
+    if now.hour < WORK_START:
+        return False
+
+    if now.hour >= WORK_END:
+        return False
+
+    return True
