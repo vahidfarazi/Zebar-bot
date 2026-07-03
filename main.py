7db7c51fdfc8ff10626ac1e834fa2a8966e2eaa6
vahidfarazi <dr.vahidfarazi@gmail.com>
2026-07-03 09:42:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,30 @@ app = Flask(__name__)
 BOT_TOKEN = "556379301:NglRetfSzjd1xWGqgyA4De3IzlNHheJB98s"
 BASE_URL = f"https://tapi.bale.ai/bot{BOT_TOKEN}"
 
+# ارسال پیام
 def send_message(chat_id, text):
-    requests.post(BASE_URL + "/sendMessage", json={
-        "chat_id": chat_id,
-        "text": text
-    })
+    requests.post(
+        BASE_URL + "/sendMessage",
+        json={
+            "chat_id": chat_id,
+            "text": text
+        }
+    )
 
+# تست اجرا
 @app.route("/", methods=["GET"])
 def home():
     return "Bot is running"
 
+# دریافت پیام از بله
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    data = request.json
-
-    message = data.get("message", {})
-    text = message.get("text", "")
-    chat_id = message.get("chat", {}).get("id")
-
-    if text and chat_id:
-        send_message(chat_id, "✅ دریافت شد: " + text)
+    print("=================================")
+    print("NEW REQUEST FROM BALE")
+    print(request.json)
+    print("=================================")
 
     return "ok"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
