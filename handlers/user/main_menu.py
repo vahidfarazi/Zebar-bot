82b2f6cd20bfca222ae3b1c3262adf9682c31395
vahidfarazi <dr.vahidfarazi@gmail.com>
2026-07-06 00:29:59 +0330

    Create main_menu.py

---
+++ handlers/user/main_menu.py
@@ -0,0 +1,31 @@
+"""
+handlers/user/main_menu.py
+
+Main menu handler.
+"""
+
+from menus import MAIN_MENU
+
+from user_state import reset
+
+
+# -----------------------------
+# Main Menu
+# -----------------------------
+def handle_main_menu(
+    chat_id: int,
+):
+    """
+    Show main menu.
+    """
+
+    # خروج از هر فرم نیمه‌کاره
+    reset(chat_id)
+
+    return {
+        "text": (
+            "به سامانه خدمات مشترکین آذرخش خوش آمدید.\n\n"
+            "لطفاً یکی از گزینه‌های زیر را انتخاب کنید."
+        ),
+        "keyboard": MAIN_MENU,
+    }
