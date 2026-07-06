e20204b6e3f84155c2edbb0d5b83bcf9252d83a0
vahidfarazi <dr.vahidfarazi@gmail.com>
2026-07-06 09:52:35 +0330

    Update notification_service.py

--- notification_service.py
+++ notification_service.py
@@ -6,17 +6,24 @@ Notification service.
 
 from config import Config
 
-from bale_client import send_message
+from bale.messages import send_message
 
 from logger import log_info
 
 
 # -----------------------------
-# Expert Group
+# Notify Expert Group
 # -----------------------------
 def notify_experts(
     message: str,
+    inline_markup=None,
 ):
+    """
+    Send message to expert group.
+
+    Returns:
+        Bale API response.
+    """
 
     group_id = Config.get_str(
         "EXPERT_GROUP_ID",
@@ -24,35 +31,64 @@ def notify_experts(
     )
 
     if not group_id:
-        return
+        return None
 
-    send_message(
-        int(group_id),
-        message,
+    result = send_message(
+
+        chat_id=int(group_id),
+
+        text=message,
+
+        inline_markup=inline_markup,
+
     )
 
     log_info(
+
         "notification",
+
         "expert_group",
+
         "sent",
+
     )
+
+    return result
 
 
 # -----------------------------
-# User
+# Notify User
 # -----------------------------
 def notify_user(
     chat_id: int,
     message: str,
+    reply_markup=None,
 ):
+    """
+    Send message to user.
 
-    send_message(
-        chat_id,
-        message,
+    Returns:
+        Bale API response.
+    """
+
+    result = send_message(
+
+        chat_id=chat_id,
+
+        text=message,
+
+        reply_markup=reply_markup,
+
     )
 
     log_info(
+
         "notification",
+
         "user",
+
         str(chat_id),
+
     )
+
+    return result
