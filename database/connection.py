c8d0adc4de08f5251a0115c39fdc4d65ba7956a0
vahidfarazi <dr.vahidfarazi@gmail.com>
2026-07-10 17:19:18 +0330

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -19,6 +19,8 @@ def get_connection() -> sqlite3.Connection:
 
     os.makedirs(DB_DIRECTORY, exist_ok=True)
 
+    print("DB PATH:", DB_PATH)
+    
     connection = sqlite3.connect(
         DB_PATH,
         check_same_thread=False,
